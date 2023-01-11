755b5859e57b6cb8ab0c1e04931c15c76dd48623
bongoSLAP <54848093+bongoSLAP@users.noreply.github.com>
2023-01-11 15:40:06 +0000

    gets value of input on click
    populates result with returned value from hello function

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const { hello } = require('./script.js');
+function hello(num) {
+    return num;
+}
 
 const DecToBinSubmit = document.querySelector('.dec-to-bin-submit');
 const BinToDecSubmit = document.querySelector('.bin-to-dec-submit');
 
+const DecToBinField = document.querySelector('.dec-to-bin-field');
+const BinToDecField = document.querySelector('.bin-to-dec-field');
+
+const DecToBinResult = document.querySelector('.dec-to-bin-result');
+const BinToDecResult = document.querySelector('.bin-to-dec-result');
+
+
 DecToBinSubmit.addEventListener('click', () => {
-    hello();
+    let value = DecToBinField.value;
+    DecToBinResult.innerHTML = hello(value);
 })
 
 BinToDecSubmit.addEventListener('click', () => {
-    hello();
+    let value = BinToDecField.value;
+    BinToDecResult.innerHTML = hello(value);
 })
