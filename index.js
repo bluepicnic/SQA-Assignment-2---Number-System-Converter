3c0fd684673f453a265614927f735038e5cc6dc3
lukejs01 <79159403+lukejs01@users.noreply.github.com>
2023-01-12 14:19:25 +0000

    Added a few more tests and code for validation

--- index.js
+++ index.js
@@ -7,7 +7,22 @@ const BinToDecSubmit = document.querySelector('.bin-to-dec-submit');
 const BinToDecField = document.querySelector('.bin-to-dec-field');
 const BinToDecResult = document.querySelector('.bin-to-dec-result');
 
+function checkString(str) {
+  let isBinary = false;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == "0" || str[i] == "1") {
+      isBinary = true;
+    } else {
+      isBinary = false;
+    }
+  }
+  return isBinary;
+}
+
 function BinToDecConversion(value) {
+  if (!checkString(value)) {
+      return "Error, enter a valid input"
+    }
   return parseInt(value, 2);
 }
 
@@ -18,17 +33,16 @@ function DecToBinConversion(value) {
 if (DecToBinSubmit != null && DecToBinField != null && DecToBinResult != null) {
   DecToBinSubmit.addEventListener('click', () => {
     let value = DecToBinField.value;
-    DecToBinResult.innerHTML = hello(value);
+    DecToBinResult.innerHTML = DecToBinConversion(value);
   })
 }
 
 if (BinToDecSubmit != null && BinToDecField != null && BinToDecResult != null) {
   BinToDecSubmit.addEventListener('click', () => {
     let value = BinToDecField.value;
-    BinToDecResult.innerHTML = hello(value);
+      BinToDecResult.innerHTML = BinToDecConversion(value);
   })
 }
-
 
 if (typeof module !== 'undefined') {
   module.exports = { BinToDecConversion };
